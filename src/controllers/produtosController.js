9e442d6e89d92159d0d579d25254de36c1e74134
Lara Granato <laragrnato17@gmail.com>
2026-03-12 14:24:38 -0300

    feat(produtos): implementa GET /produtos/:id com tratamento de 404

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -5,10 +5,18 @@ function listar(req, res) {
   res.status(200).json(produtos);
 }
 
-
 function buscarPorId(req, res) {
-  // TODO
+  const id = parseInt(req.params.id);
+
+  const produto = produtos.find(p => p.id === id);
+
+  if (!produto) {
+    return res.status(404).json({ erro: "Produto não encontrado" });
+  }
+
+  res.status(200).json(produto);
 }
+
 
 function criar(req, res) {
   // TODO
